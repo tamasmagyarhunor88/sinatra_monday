61275aa540addafdb6f0e36b1bdbd09c9ef61827
Josué Estévez Fernández <jestfer6@gmail.com>
2018-02-05 17:20:57 +0000

    sorting gems alphabetically and adding information to keep session secret

--- app.rb
+++ app.rb
@@ -1,17 +1,23 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
-  "Hello Josu and Hunor and world!"
+  'Hello Josu and Hunor and world!'
 end
 
 get '/secret' do
-  "password to NASA is = 12345"
+  'password to NASA is = 12345'
 end
 
 get '/makers' do
-  "Makers Academy week 3"
+  'Makers Academy week 3'
 end
 
 get '/London' do
-  "lets see whats going on"
+  'lets see whats going on'
+end
+
+get '/shotgun' do
+  'just trying shotgun'
 end
