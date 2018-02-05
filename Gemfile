b97db665e208b14c9e9a45da30a9a75a6de7b4d6
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-05 17:00:09 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+ruby '2.5.0'
+
+gem 'sinatra'
+gem 'shotgun'
+
---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+  "Hello Josu and Hunor and world!"
+end
+
+get '/secret' do
+  "password to NASA is = 12345"
+end
+
+get '/makers' do
+  "Makers Academy week 3"
+end
+
+get '/London' do
+  "lets see whats going on"
+end
