9b179b027276a345e8ae2c44a60268b566f12446
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-06 08:53:00 +0000

    add cat

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
+
 get '/' do
   "Hello Josu and Hunor and world!"
 end
