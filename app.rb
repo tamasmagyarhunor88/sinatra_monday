52362e946b7ebd9dd6455b664cf7f22ebf666c28
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-06 09:48:50 +0000

    add /home page

--- app.rb
+++ app.rb
@@ -3,31 +3,9 @@ require 'sinatra'
 set :session_secret, 'super secret'
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
 
-get '/' do
-  'Hello Josu and Hunor and world!'
-end
-
-get '/secret' do
-  'password to NASA is = 12345'
-end
-
-get '/makers' do
-  'Makers Academy week 3'
-end
-
-get '/London' do
-  'lets see whats going on'
-end
-
-get '/shotgun' do
-  'just trying shotgun'
-end
-
-get '/whatever' do
-  "testing shotgun for life"
+get '/home' do
+  erb(:home)
 end
