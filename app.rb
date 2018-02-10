b78596c3a496a86346274d3830794d07334336c4
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-02-10 16:12:55 +0000

    challenge cat params done

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
-get '/cat' do
+get '/random_cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
